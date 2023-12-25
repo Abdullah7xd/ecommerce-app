6b233339b834830c26f49e0e22f2849a50225eab
Abdullah <abdullahpoi@gmail.com>
2023-12-25 12:34:42 +0530

    ...

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,7 @@
+// import userModel from "../models/userModel.js"
+// import userModel from "../config/models/userModel.js";
+import userModel from "../models/userModel.js";
+import { hashPassword } from "./../helper/authHelper.js";
 export const registerController = async(req,res) =>{
     try {
        const {name,email,password,phone,address}= req.body
@@ -5,6 +9,42 @@ export const registerController = async(req,res) =>{
        if(!name){
         return res.send({error: 'Name is required'})
        } 
+       if(!email){
+        return res.send({error: 'email is required'})
+       } 
+       if(!password){
+        return res.send({error: 'password is required'})
+       } 
+       if(!phone){
+        return res.send({error: 'phone is required'})
+       } 
+       if(!address){
+        return res.send({error: 'address is required'})
+       }
+       const existingUser = await userModel.findOne({email})
+       //existing user
+       if(existingUser){
+        return res.status(200).send({
+            success: true,
+            message:'Already Register please login',
+        })
+       } 
+       //register user
+       const hashedPassword = await hashPassword(password);
+       //save password
+       const user = await new userModel({
+        name,
+        email,
+        phone,
+        address,
+        password: hashedPassword,
+       }).save()
+
+       res.status(201).send({
+        success:true,
+        message:'user Register Successfully',
+        user,
+       })
     } catch (error) {
         console.log(error);
         res.status(500).send({
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -26,10 +26,7 @@ const userSchema = new mongoose.Schema({
     role:{
         type:Number,
         default:0
-    },
-    
-
+    }
 },{timestamps:true}
 );
-
-export default mongoose.model('users',userSchema)
+  export default mongoose.model('users',userSchema)
