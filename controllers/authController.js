e929e9b50d164a15b4a4990cbd13f200cbf63696
Abdullah <abdullahpoi@gmail.com>
2023-12-26 23:18:06 +0530

    ...

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,9 @@
 // import userModel from "../models/userModel.js"
 // import userModel from "../config/models/userModel.js";
 import userModel from "../models/userModel.js";
-import { hashPassword } from "./../helper/authHelper.js";
+import router from "../routes/authRoute.js";
+import { comparePassword, hashPassword } from "./../helper/authHelper.js";
+import JWT from "jsonwebtoken";
 export const registerController = async(req,res) =>{
     try {
        const {name,email,password,phone,address}= req.body
@@ -57,3 +59,42 @@ export const registerController = async(req,res) =>{
 
 };
 
+//POST LOGIN
+export const loginController = async(req,res) =>{
+    try {
+        const {email,password} = req.body
+        //validation
+        if(!email || !password){
+            return res.status(404).send({
+                success:false,
+                message:'Invalid email or password'
+            })
+        }
+        // check user
+        const user = await userModel.findOne({email})
+        if(!user){
+            return res.status(404).send({
+                success:false,
+                message:'Email is not registered'
+            })
+        }
+        //token
+        const token = await JWT.sign({id})
+        const match = await comparePassword(password,user.password)//for comparing users
+        if(!match){
+            return res.status(200).send({
+                success:false,
+                message:'Invalid password'
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message:'Error in login',
+            error
+        })
+        
+    }
+
+};
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,16 @@
 import express from 'express';
-import { registerController } from '../controllers/authController.js';
+import { loginController, registerController } from '../controllers/authController.js';
 
 // Create a router object
 const router = express.Router();
 
 // Define the route for registration
 router.post('/register', registerController);
+
+//LOGIN || POST
+
+router.post('/login',loginController)
+
 
 // Export the router
 export default router;
